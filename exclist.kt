09a18d7c97cc8a4abfdb3b5e8ab0e7c47ad94925
leexfranco <82109766+leexfranco@users.noreply.github.com>
2022-03-03 16:13:58 -0300

    Add files via upload

---
+++ exclist.kt
@@ -0,0 +1,31 @@
+fun main () {
+    val nomeReference1 = mutableListOf<String>()
+    while (true) {
+
+        println("Mercearia Van Gogh")
+        println("1 - Adicione produtos no estoque")
+        println("2 - Remova produtos do estoque")
+        println("3 - Atualize um item")
+        println("4 - Veja a lista do estoque")
+        println("5 - Para sair")
+
+        println("Escolha uma opção")
+        val opc = readLine()!!..toInt()
+
+        when (opc) {
+
+
+            1 -> {
+                print("Digite o item que vai ser incluido no estoque")
+                val item = readLine()
+
+
+
+            }
+
+
+            }
+        }
+
+    }
+}
